dff2a14c80d0a272f96672ed233a0d08fb3b53a2
Oleksandr Strakhov <mrallexus@gmail.com>
2023-01-11 00:49:01 +0200

    Configure CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,19 @@
 import Fastify from 'fastify';
-
-const fastify = Fastify({
-  logger: true,
-});
-
-fastify.get('/ping', async (request, reply) => {
-  return { status: 'ok' };
-});
+import cors from '@fastify/cors'
 
 const start = async () => {
+  const fastify = Fastify({
+    logger: true,
+  });
+
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.register(cors);
+    
+    fastify.get('/ping', async (request, reply) => {
+      return { status: 'ok' };
+    });
+
+    await fastify.listen({ port: 4000 });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
