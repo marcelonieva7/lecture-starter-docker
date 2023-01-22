007ce7300e0abfd65637f8842749bf7b6e1f2e89
Oleksandr Strakhov <mrallexus@gmail.com>
2023-01-22 12:28:33 +0200

    Add database support for backend

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -6,4 +6,11 @@ export const config = {
   http: {
     port: Number(process.env.HTTP_PORT) || 4000,
   },
+  database: {
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: Number(process.env.POSTGRES_PORT) || 5432,
+    user: process.env.POSTGRES_USER || 'postgres',
+    password: process.env.POSTGRES_PASSWORD || 'secret',
+    database: process.env.POSTGRES_DATABASE || 'postgres',
+  },
 };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors'
 import { config } from './config';
+import { database } from './database';
 
 const start = async () => {
   const fastify = Fastify({
@@ -10,8 +11,20 @@ const start = async () => {
   try {
     await fastify.register(cors);
 
-    fastify.get('/ping', async (request, reply) => {
-      return { status: 'ok' };
+    fastify.get('/status', async (request, reply) => {
+      let databaseOk = false;
+
+      try {
+        await database.raw('SELECT 1');
+        databaseOk = true;
+      } catch (e) {
+        fastify.log.error(e);
+      }
+
+      return {
+        http: 'ok',
+        database: databaseOk ? 'ok' : 'error',
+      };
     });
 
     await fastify.listen({ port: config.http.port });
